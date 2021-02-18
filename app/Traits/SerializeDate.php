c041cdb161aa92ead97b426eebfeb907516ed5e1
Rommy Gustiawan <rgcppp@gmail.com>
2021-02-18 23:12:46 +0700

    Memformat dokumen

--- app/Traits/SerializeDate.php
+++ app/Traits/SerializeDate.php
@@ -6,6 +6,12 @@ use DateTimeInterface;
 
 trait SerializeDate
 {
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
     protected function serializeDate(DateTimeInterface $date)
     {
         return $date->format('Y-m-d H:i:s');
